e0736f9b850d8235b50764d34ffe475a642ff1b1
Dreamin121 <92585241+Dreamin121@users.noreply.github.com>
2023-03-09 16:31:20 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -7,5 +7,5 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `
-- 欢迎访问 [GCURobot](https://gcubot.cn) Power By WolfSoft
+- 欢迎访问 [GCURobot](https://gcubot.cn) --Power By WolfSoft
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
