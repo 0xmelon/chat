6b0eb2241bff807781645d54328022c12a3a29f5
Dreamin121 <92585241+Dreamin121@users.noreply.github.com>
2023-03-09 09:54:24 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -7,5 +7,5 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `
-- 查看 [主站](https://drz.ink)，访问 [GPT](https://chat.drz.ink)
+- 欢迎访问 [主站](https://drz.ink)，访问 [GPT](https://chat.drz.ink)
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
