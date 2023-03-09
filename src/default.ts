499368a90d500f7446c8e40d9c1ba6b00d464750
Dreamin121 <92585241+Dreamin121@users.noreply.github.com>
2023-03-09 09:50:46 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -7,7 +7,5 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `
-- 本站仅用于演示，填入自己的 key 才可使用。
-- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
-- 由 [ourongxing](https://github.com/ourongxing) 修改自 [Diu](https://github.com/ddiu8081/chatgpt-demo)，查看 [源码](https://github.com/ourongxing/chatgpt-vercel)，欢迎自部署。
+- 查看 [主站](https://drz.ink)，访问 [GPT](https://chat.drz.ink)
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
